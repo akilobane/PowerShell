135988e2e9678ae2c7e6d57ca9c6a401dbe45f8a
akilobane <78292954+akilobane@users.noreply.github.com>
2021-08-28 01:17:16 +1000

    Update 1
    Just explained a little bit more.

--- SortDesktopInOrder.ps1
+++ SortDesktopInOrder.ps1
@@ -1,2 +1,3 @@
 Get-ChildItem -Path Users/<user>/Desktop | Sort-Object
 <# Get-ChildItem = collect items, does not show empty directories#> 
+<# Sort-Object = objects sorted NOTE: Always in Ascending unless specified otherwise. #>
