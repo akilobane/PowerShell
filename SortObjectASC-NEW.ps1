af13452695894d8a48015e037e7db27152fe2cf7
akilobane <78292954+akilobane@users.noreply.github.com>
2021-09-07 17:01:54 +1000

    Create SortObjectASC-NEW.ps1

---
+++ SortObjectASC-NEW.ps1
@@ -0,0 +1,3 @@
+Get-ChildItem -Path C:\Users\<user>\<directory> | Sort-Object 
+<# Get-ChildItem = collect items, does not show empty directories#> 
+<# Sort-Object = objects sorted NOTE: Always in Ascending unless specified otherwise. #>
