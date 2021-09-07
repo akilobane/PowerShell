af65ce8afddbfb3080585abf43c04f33f0be33cf
Emil James Reyes <james.reyes92@live.com>
2021-09-07 16:30:06 +1000

    Revision 1
    changed the directory

--- SortDesktopInOrder copy.ps1
+++
@@ -1,3 +0,0 @@
-Get-ChildItem -Path Users/<user>/Desktop | Sort-Object
-<# Get-ChildItem = collect items, does not show empty directories#> 
-<# Sort-Object = objects sorted NOTE: Always in Ascending unless specified otherwise. #>
