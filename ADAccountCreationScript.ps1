8979bd7c8b4d94c475c27f60007e37e3fb81c309
Emil James Reyes <james.reyes92@live.com>
2021-09-15 15:41:19 +1000

    Prototype Script

---
+++ ADAccountCreationScript.ps1
@@ -0,0 +1,16 @@
+﻿New-ADUser `
+-Name "Anakin Skywalker" `
+-GivenName "Anakin" `
+-Surname "Skywalker" `
+-SamAccountName "askywalker" `
+-ChangePasswordAtLogon $true `
+-Department "Jedi Order" `
+-Title "Jedi Knight" `
+-EmailAddress "askywalker@gmail.com" `
+-Description "Was given a seat on the high council but not the rank of master" `
+-Enabled $true `
+-AccountPassword (Read-Host -AsSecureString "Input User Password") `
+-Path "OU=Home,DC=Labyrinth,DC=local"
+ 
+<# Booleans are created like variables e.g $true/$false #>
+<# Read-Host asks for user input. -AsSecureString masks input as *** #>
